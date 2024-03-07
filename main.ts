0fbec212aad8fe40b0b320109482e623a381df99
Wajids36 <161895021+Wajids36@users.noreply.github.com>
2024-03-07 09:42:24 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,85 @@
+// Project_00
+
+// console.log("hello world"); 
+
+// Path: main.ts
+
+// Project_01
+// lesson_01
+
+// let message = "Hello world";
+// console.log(message);
+
+// lesson_02
+
+// var, l det, constc d
+
+// var allow to code same variable  
+
+// var firstName= "hamza";
+
+// var firstName= "ali"
+
+// console.log(firstName)
+
+// let allow to code change variable  data
+
+// let firstName= "hamza";
+
+// firstName= "ali"
+
+// console.log(firstName)
+
+// const do not allow to code change variable  data
+
+// const firstName= "hamza";
+
+// firstName= "ali"
+
+// console.log(firstName)
+
+
+
+
+// let message: string = 'hello world'
+// message = 123
+// console.log(message);
+// let age: number= 23;
+
+
+// let firstName: string = "Ammar"
+// let age: number = 23;
+// let isMarried: boolean = true;
+
+
+// string+string=string
+// number+string=string
+// string+Number=string
+// number+number=number
+
+// let num1 = 1;
+// let num2 = 1;
+// console.log(num1 + num2);
+
+
+// let myName= "wajid";
+// let message= "hello"+myName
+// console.log(message)
+
+// let str1 = "1";
+// let str2 = 2;
+// console.log(str1+str2)
+
+
+// let wajid = 9;
+// let str2 = 2;
+// console.log(wajid/str2)
+
+// let wajid = 100
+// let fayaz = 50
+// console.log(wajid+fayaz);
+
+// let num1: number = 3;
+// let num2: number = 2;
+// console.log(num1**num2);
+
